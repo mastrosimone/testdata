fda6c10c74d2bcbc88238727d74cb06598cc1e53
mastrosimone <ale.mastrosimone@gmail.com>
2018-06-09 16:24:50 +0200

    09 giugno 2018

---
+++ htmlgenerator/api.js
@@ -0,0 +1,102 @@
+var api = {
+    en: {
+         menu: {
+             data: [
+                 {
+                     label: 'Menu 1',
+                     href: '#menu1'
+                 },
+                 {
+                     label: 'Menu 2',
+                     href: '#menu2'
+                 },
+                 {
+                     label: 'Cle',
+                     href: '#menu3'
+                 }
+             ]
+         },
+         personal: {
+             section: {},
+             data: {
+                 firstname: 'Alessio',
+                 surname: 'Mastrosimone',
+                 position: 'jobposition',
+                 img: 'https://www.placecage.com/500/500',
+                 skills: [
+                     'skill1',
+                     'skill2',
+                     'skill3',
+                     'skill4'
+                 ]
+             }
+         },
+         works: {
+             section: {
+                 title:'Work Experience'
+             },
+             data: [
+                 {
+                     title: 'Ui Engineer @mosaicoon',
+                     description: 'ita'
+                 },
+                 {
+                    title: 'Ui Engineer @mosaicoon',
+                    description: 'ita'
+                },
+                {
+                   title: 'Ui Engineer @mosaicoon',
+                   description: 'ita'
+               },
+               {
+                  title: 'Ui Engineer @mosaicoon',
+                  description: 'ita'
+              }
+             ]
+         }
+    },
+     it: {
+         menu: {
+             data: [
+                 {
+                     label: 'Menu 1',
+                     href: '#menu1'
+                 },
+                 {
+                     label: 'Menu 2',
+                     href: '#menu2'
+                 },
+                 {
+                     label: 'Cle',
+                     href: '#menu3'
+                 }
+             ]
+         },
+         personal: {
+             section: {},
+             data: {
+                 firstname: 'Name en',
+                 surname: 'Alessio',
+                 position: 'Mastrosimone',
+                 img: 'https://www.placecage.com/500/500',
+                 skills: [
+                     'skill1',
+                     'skill1',
+                     'skill1',
+                     'skill1'
+                 ]
+             }
+         },
+         works: {
+             section: {
+                 title:'Work Experience'
+             },
+             data: [
+                 {
+                     title: 'Ui Engineer @mosaicoon',
+                     description: 'ita'
+                 }
+             ]
+         }
+    },
+};
