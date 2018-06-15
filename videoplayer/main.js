dcab4b956ba06fceb92504c9bfb027612287f623
mastrosimone <ale.mastrosimone@gmail.com>
2018-06-15 16:55:29 +0200

    15 giugno

--- videoplayer/main.js
+++ videoplayer/main.js
@@ -1,79 +1,79 @@
 var api = { data:
                 [ 
                     { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
                     "subtitle" : "By Blender Foundation",
                     "thumb" : "images/BigBuckBunny.jpg",
                     "title" : "Big Buck Bunny"
                     },
                     { "description" : "The first Blender Open Movie from 2006",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
                     "subtitle" : "By Blender Foundation",
                     "thumb" : "images/ElephantsDream.jpg",
                     "title" : "Elephant Dream"
                     },
                     { "description" : "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
                     "subtitle" : "By Google",
                     "thumb" : "images/ForBiggerBlazes.jpg",
                     "title" : "For Bigger Blazes"
                     },
                     { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
                     "subtitle" : "By Google",
                     "thumb" : "images/ForBiggerEscapes.jpg",
                     "title" : "For Bigger Escape"
                     },
                     { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
                     "subtitle" : "By Google",
                     "thumb" : "images/ForBiggerFun.jpg",
                     "title" : "For Bigger Fun"
                     },
                     { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
                     "subtitle" : "By Google",
                     "thumb" : "images/ForBiggerJoyrides.jpg",
                     "title" : "For Bigger Joyrides"
                     },
                     { "description" :"Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.", 
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
                     "subtitle" : "By Google",
                     "thumb" : "images/ForBiggerMeltdowns.jpg",
                     "title" : "For Bigger Meltdowns"
                     },
                     { "description" : "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
                     "subtitle" : "By Blender Foundation",
                     "thumb" : "images/Sintel.jpg",
                     "title" : "Sintel"
                     },
                     { "description" : "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
                     "subtitle" : "By Garage419",
                     "thumb" : "images/SubaruOutbackOnStreetAndDirt.jpg",
                     "title" : "Subaru Outback On Street And Dirt"
                     },
                     { "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
                     "subtitle" : "By Blender Foundation",
                     "thumb" : "images/TearsOfSteel.jpg",
                     "title" : "Tears of Steel"
                     },
                     { "description" : "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
                     "subtitle" : "By Garage419",
                     "thumb" : "images/VolkswagenGTIReview.jpg",
                     "title" : "Volkswagen GTI Review"
                     },
                     { "description" : "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
                     "subtitle" : "By Garage419",
                     "thumb" : "images/WeAreGoingOnBullrun.jpg",
                     "title" : "We Are Going On Bullrun"
                     },
                     { "description" : "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-                    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ],
+                    "sources" : "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
                     "subtitle" : "By Garage419",
                     "thumb" : "images/WhatCarCanYouGetForAGrand.jpg",
                     "title" : "What care can you get for a grand?"
@@ -82,40 +82,168 @@ var api = { data:
             };
 
 var pre = [] //dichiara la variabile esterna per il pulsante previous
+var shuffleplay = false
+var playstatus = false
+var devmode = false
+
+for(x = 0;x < api.data.length;x++)
+var current = 0
+/* var title = api.data[current].title
+var subtitle = api.data[current].subtitle
+var thumb = api.data[current].thumb
+var description = api.data[current].description
+var sources = api.data[current].sources */
+
 
 var Player = (function(api) {
 
-    var video = document.querySelector("#video");
-    var url = document.querySelector("#url");
-    
+    var video = document.querySelector("#video")
+    var url = document.querySelector("#url")
+
+    //ok
     function Play() {
-    
-        return video.play()
+        pre = source.src
+        if (devmode == true){
+            console.log('playstatus: ', playstatus)
+            console.log('Precedente: ', pre)
+        }
+        if (playstatus == false){
+            playstatus = true
+        return video.play(), PlayBottonTransform(), ViewData()
+        }
+        if (playstatus == true){
+            playstatus = false
+        return video.pause(), PlayBottonTransform(), ViewData()
+        }
     }
-    function Pause() {
     
-        return video.pause()
+    //ok
+    function PlayBottonTransform() {
+        
+        var buttonplay = document.getElementById('play')
+        var playicon = document.getElementById('playicon')
+        
+        if (playstatus == true){
+            buttonplay.classList.replace('play', 'pause')
+            playicon.setAttribute('class', 'fa fa-pause')
+        }
+        if (playstatus == false){
+            buttonplay.classList.replace('pause', 'play')
+            playicon.setAttribute('class', 'fa fa-play')
+        }
     }
+
+    function DevMode() {
+        if (devmode == false){
+            devmode = true
+            console.log('Dev Mode Actived')
+        } else {
+            devmode = false
+            console.log('Dev Mode Deactived')
+        }
+    }
+
+    function ViewData() {
+
+        var title = api.data[current].title
+        titlehtml = document.getElementById('video_title')
+        titlehtml.textContent = title
+
+        var sources = api.data[current].sources
+        sourceshtml = document.getElementById('source')
+        sourceshtml.setAttribute('src', sources)
+
+        var thumb = api.data[current].thumb
+        thumbhtml = document.getElementById('video_thumb')
+        thumbhtml.setAttribute('src', thumb)
+
+        var description = api.data[current].description
+        descriptionhtml = document.getElementById('video_description')
+        descriptionhtml.textContent = description
+
+        var subtitle = api.data[current].subtitle
+        subtitlehtml = document.getElementById('video_subtitle')
+        subtitlehtml.textContent = subtitle
+
+        if (devmode == true){
+            console.log('Titolo: ', title)
+            console.log('Sorgente: ', sources)
+            console.log('Anteprima: ', thumb)
+            console.log('Sottotitolo: ', subtitle)
+            console.log('Descrizione: ', description)
+        }
+    }
+// da sistemare
+    function AllData() {
+        var result = []
+        for(x = 0;x < api.data.length;x++){
+            title = api.data[x].title
+            console.log(title)
+        }
+        var current = Math.floor(Math.random()*api.data.length)
+        var subtitle = api.data[x].subtitle
+        var thumb = api.data[x].thumb
+        var description = api.data[x].description
+
+        var title = api.data[x].title
+        titlehtml = document.getElementById('video_title')
+        titlehtml.textContent = title
+
+        var sources = api.data[x].sources
+        sourceshtml = document.getElementById('source')
+        sourceshtml.setAttribute('src', sources)
+    }
+
+    function Random() {
+        current = Math.floor(Math.random()*api.data.length)
+        
+        return video.load(), Play(), console.log('Current Video is: ', current)
+        
+    }
+
+    function Shuffle() {
+
+        var shuffle = document.getElementById('shuffle')
+        if (shuffleplay == false){
+            shuffleplay = true
+            shuffle.textContent = "Shuffle  On"
+            console.log('Shuffle: ', shuffleplay)
+           } else {
+            shuffleplay = false;
+            shuffle.textContent = "Shuffle  Off"
+            console.log('Shuffle: ', shuffleplay)
+           }  
+    }
+
+    function List() {
+        // da richiamare con Player.getList()
+        console.log(api)
+    } 
+
     function Up() {
+        if (devmode == true){
         console.log(video.volume)
+        }
         var up = document.getElementById('up')
         var down = document.getElementById('down')
         down.classList.remove("down_ko")
         if (video.volume < 0.9){
-            Math.round(video.volume+=0.1)}
-            up.classList.remove("up_ko")
+            video.volume+=0.1
+            up.classList.remove("up_ko")}
         if (video.volume > 0.8){
             up.classList.add("up_ko")
         }
         
     }
     function Down() {
-        console.log(video.volume)
+        if (devmode == true){
+            console.log(video.volume)
+            }
         var up = document.getElementById('up')
         var down = document.getElementById('down')
         up.classList.remove("up_ko")
         if (video.volume > 0.1){
-            Math.round(video.volume-=0.1)
+            video.volume-=0.1
             down.classList.remove("down_ko")}
         if (video.volume < 0.1){
             var up = document.getElementById('up')
@@ -125,7 +253,6 @@ var Player = (function(api) {
     }
     function Apri() {
         var source = document.getElementById('source')
-        console.log(source)
         source.setAttribute('src', url.value)
         video.load();
         console.log(source)
@@ -152,22 +279,47 @@ var Player = (function(api) {
 
     }
     function Next() {
+        if (shuffleplay == false) {
+            if (current == x) {
+                current = 0
+            } else {
+            current = current + 1
+            }
+            return video.load(), Play()
+        }
+        if (shuffleplay == true) {
+            return video.load(), Random()
+        }
+
+    }
+/*     function Next() {
         var result = []
         for(x = 0;x < api.data.length;x++)
         
-        result.push( api.data[x].sources)
+        result.push(api.data[x].sources)
+        
+        console.log(result)
+        var item = result[Math.floor(Math.random()*api.data.length)]
+        title = []
+        title.push(api.data.title)
         var item = result[Math.floor(Math.random()*api.data.length)]
         var source = document.getElementById('source')
-        pre = source.src;
+        pre = source.src
         source.setAttribute('src', item)
+        var titleonvideo = []
+        for(x = 0;x < api.data.length;x++)
+        titleonvideo = api.data[x].title
+        var videotext = document.getElementById('video_title')
+        console.log(titleonvideo)
+        console.log(videotext)
+        videotext.textContent = titleonvideo
         video.load();
 
     }
+    */
     function Previous() {
-        var source = document.getElementById('source')
         source.setAttribute('src', pre)
-        console.log(pre)
-        video.load();
+        return video.load(), Play()
         
     }
     function Coda() {
@@ -186,46 +338,9 @@ var Player = (function(api) {
         a.click() */
 
     }
-    function Shuffle() {
-        var result = []
-    
-        for(x = 0;x < api.data.length;x++
-        )
-        
-        result.push(api.data[x].sources)
-        var item = result[Math.floor(Math.random()*api.data.length)]
-        var source = document.getElementById('source')
-        var random = document.getElementById('shuffle')
-        source.setAttribute('src', item)
-        random.textContent = "Random"
-        pre = item //dichiara la variabile esterna per il pulsante previous
-        video.load();
-
-
-/*         var z = []
-        var source = document.getElementById('source')
-        var a = document.createElement("a")
-        a.setAttribute('href', source.src)
-        a.setAttribute('targer', "_blank")
-        a.click() */
-
-    }
-
-/* OK!
-    function Shuffle() {
-        var result = []
-    
-        for(x = 0;x < api.data.length;x++)
-        
-       result.push( api.data[x].sources)
-        console.log(result)
-
-    } */
-
-    
 
     return { 
-       getPlay: Play, getPause: Pause, getUp: Up, getDown: Down, getApri: Apri, getBarenable: Barenable, getDownload: Download, getCoda: Coda, getShuffle: Shuffle, getNext: Next, getPrevious: Previous
+       getPlay: Play, getUp: Up, getDown: Down, getApri: Apri, getBarenable: Barenable, getDownload: Download, getCoda: Coda, getShuffle: Shuffle, getNext: Next, getPrevious: Previous, getRandom: Random, getList: List, getPlayBottonTransform: PlayBottonTransform, getViewData: ViewData, getDevMode: DevMode
     };
 
 
@@ -237,8 +352,6 @@ var App = (function(Player) {
     
     var Play = document.querySelector('#play')
     Play.addEventListener('click', Player.getPlay)
-    var Pause = document.querySelector('#pause')
-    Pause.addEventListener('click', Player.getPause)
     var Up = document.querySelector('#up')
     Up.addEventListener('click', Player.getUp)
     var Down = document.querySelector('#down')
@@ -257,4 +370,6 @@ var App = (function(Player) {
     Next.addEventListener('click', Player.getNext)
     var Previous = document.querySelector('#previous')
     Previous.addEventListener('click', Player.getPrevious)
+    var DevButton = document.querySelector('#devbutton')
+    DevButton.addEventListener('click', Player.getDevMode)
 })(Player);
